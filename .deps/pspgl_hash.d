9587eebe186583aba0cced769b7d24ca2f01ba2b
Daniel <dwelch.appleid@gmail.com>
2026-06-26 13:43:04 +0100

    Add aggregate profiling infrastructure

---
+++ .deps/pspgl_hash.d
@@ -0,0 +1,17 @@
+pspgl_hash.o: pspgl_hash.c /home/dwelch/pspdev/psp/include/stdlib.h \
+ /home/dwelch/pspdev/psp/include/machine/ieeefp.h \
+ /home/dwelch/pspdev/psp/include/_ansi.h \
+ /home/dwelch/pspdev/psp/include/newlib.h \
+ /home/dwelch/pspdev/psp/include/_newlib_version.h \
+ /home/dwelch/pspdev/psp/include/sys/config.h \
+ /home/dwelch/pspdev/psp/include/sys/features.h \
+ /home/dwelch/pspdev/lib/gcc/psp/15.2.0/include/stddef.h \
+ /home/dwelch/pspdev/psp/include/sys/reent.h \
+ /home/dwelch/pspdev/psp/include/_ansi.h \
+ /home/dwelch/pspdev/psp/include/sys/cdefs.h \
+ /home/dwelch/pspdev/psp/include/machine/_default_types.h \
+ /home/dwelch/pspdev/psp/include/sys/_types.h \
+ /home/dwelch/pspdev/psp/include/machine/_types.h \
+ /home/dwelch/pspdev/psp/include/sys/lock.h \
+ /home/dwelch/pspdev/psp/include/machine/stdlib.h \
+ /home/dwelch/pspdev/psp/include/alloca.h pspgl_hash.h
